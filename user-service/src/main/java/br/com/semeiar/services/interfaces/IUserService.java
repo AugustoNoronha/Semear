5348f255247599087565976e9c0298b6140c5271
Augusto Noronha Leite <augusto.leite@axis-mobfintech.com>
2025-09-18 10:00:03 -0300

    firs-users-enpoints-local-storage

---
+++ user-service/src/main/java/br/com/semeiar/services/interfaces/IUserService.java
@@ -0,0 +1,12 @@
+package br.com.semeiar.services.interfaces;
+
+import br.com.semeiar.models.User;
+
+import java.util.List;
+
+public interface IUserService {
+    User createUser(User user);
+    User getUserById(String id);
+    List<User> listUsers();
+    Boolean deleteUser(String id);
+}
